4e7841107c8c0c8598716824a934e04946056c46
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-10-30 17:06:08 -0300

    Add user input for name and age

---
+++ library.rb
@@ -0,0 +1,9 @@
+puts "Nos informe seu primeiro nome!"
+nome = gets.chomp
+puts "Nos informe seu sobrenome!"
+sobrenome = gets.chomp
+puts "Nos informe sua idade"
+idade = gets.chomp.to_i
+
+
+puts "Seu nome é #{nome} #{sobrenome} e sua idade é #{idade}"
